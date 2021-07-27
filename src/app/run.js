93daaa5df0bc5d01c3ccbb2297dfe002e9773dc0
d4nielj <d4niel.djm@gmail.com>
2021-07-27 18:35:14 -0500

    Remove unnecesary declaration

--- src/app/run.js
+++ src/app/run.js
@@ -3,8 +3,7 @@ import data from './data.js';
 import updateDOM from './updateDOM.js';
 
 const run = () => {
-  let loadedData = [];
-  loadedData = fetchData(data);
+  const loadedData = fetchData(data);
   updateDOM(loadedData);
 };
 
