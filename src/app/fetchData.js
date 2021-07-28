89948b826f09c653496ebab406480772b45a5c6f
d4nielj <d4niel.djm@gmail.com>
2021-07-28 16:32:33 -0500

    Fix linter error

--- src/app/fetchData.js
+++ src/app/fetchData.js
@@ -4,7 +4,9 @@ const fetchData = async () => {
   let data = [];
   await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9YugNr0GKrJIfPey1hC9/scores/')
     .then((response) => response.json())
-    .then((json) => data = json.result);
+    .then((json) => {
+      data = json.result;
+    });
   return data;
 };
 
