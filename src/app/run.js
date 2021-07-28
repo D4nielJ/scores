da351a27b947c883fdf31ece151cbc20dd4fb7f1
d4nielj <d4niel.djm@gmail.com>
2021-07-28 15:58:00 -0500

    Add submit function to send data to the API

--- src/app/run.js
+++ src/app/run.js
@@ -1,6 +1,7 @@
 import fetchData from './fetchData.js';
 import updateDOM from './updateDOM.js';
 import refresh from './refresh.js';
+import submit from './submit.js';
 
 const run = async () => {
   const loadedData = await fetchData();
---
+++ src/app/submit.js
@@ -0,0 +1,23 @@
+const submit = () => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', async (e) => {
+    // Fetch the data to the API
+    e.preventDefault();
+    const user = form.name.value;
+    const score = form.score.value;
+    await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/gagYyMmW80J2RAQLFXbE/scores/',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          user: user,
+          score: score
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+  });
+};
+
+export default submit;
