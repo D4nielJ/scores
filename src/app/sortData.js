0add2b56c39a1a053b418d2f2bc863501d994ede
d4nielj <d4niel.djm@gmail.com>
2021-07-29 17:57:09 -0500

    Fix linter errors

--- src/app/sortData.js
+++ src/app/sortData.js
@@ -1,7 +1,7 @@
 const sortData = (data) => {
   let placeholder = {};
-  for (let i = 0; i < data.length; i++) {
-    for (let j = i + 1; j < data.length; j++) {
+  for (let i = 0; i < data.length; i += 1) {
+    for (let j = i + 1; j < data.length; j += 1) {
       if (parseInt(data[i].score, 10) < parseInt(data[j].score, 10)) {
         placeholder = data[i];
         data[i] = data[j];
--- src/app/updateDOM.js
+++ src/app/updateDOM.js
@@ -1,4 +1,4 @@
-import sortData from './sortData';
+import sortData from './sortData.js';
 
 const updateDOM = (data) => {
   const table = document.querySelector('tbody');
